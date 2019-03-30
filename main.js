57e80be08a0106eac2351e7baa214bd5773de69a
Lakia Jones <rachandra_jones@yahoo.com>
2019-03-30 13:35:58 -0500

    SubmitFormComplete

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log('Testing')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const submitEmail = (e) => {
+    e.preventDefault();
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+};
+const submitButton = 
+document.getElementById('submitButton')
+
+const init = () => {
+    submitButton.addEventListener("click", submitEmail);
+};
+init();
